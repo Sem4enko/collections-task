b281be66760338524870a62069bdacc2e0fe343f
Max Semchenko <sem4enko_max@ukr.net>
2021-10-11 14:27:15 +0300

    Updated set

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -1,60 +1,111 @@
 package co.inventorsoft.academy.collections.model;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.function.Function;
 
 public class Range<T> implements Set<T> {
 
+    private HashSet<T> set;
+
+    private Range() {
+
+        set = new HashSet<>();
+    }
+
+
     public int size() {
-        return 0;
+        return set.size();
     }
 
     public boolean isEmpty() {
-        return false;
+        return set.isEmpty();
     }
 
     public boolean contains(Object o) {
-        return false;
+        return set.contains(o);
     }
 
     public Iterator<T> iterator() {
-        return null;
+        return set.iterator();
     }
 
     public Object[] toArray() {
-        return new Object[0];
+        return set.toArray();
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        return set.toArray(a);
     }
 
     public boolean add(T t) {
-        return false;
+        return set.add(t);
     }
 
     public boolean remove(Object o) {
-        return false;
+        return set.remove(o);
     }
 
     public boolean containsAll(Collection<?> c) {
-        return false;
+        return set.containsAll(c);
     }
 
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        return set.addAll(c);
     }
 
     public boolean retainAll(Collection<?> c) {
-        return false;
+        return retainAll(c);
     }
 
     public boolean removeAll(Collection<?> c) {
-        return false;
+        return removeAll(c);
     }
 
     public void clear() {
+        set.clear();
+    }
 
+    public static <K extends Comparable> Range<K> of(K a, K d, Function<K, K> add) {
+
+        Range<K> range = new Range<>();
+
+        if (a.compareTo(d) == 0) {
+            return range;
+        }
+        K next = a;
+        while (next.compareTo(d) <= 0) {
+            range.add(next);
+            next = add.apply(next);
+        }
+
+        return range;
+
+    }
+
+    public static Range<Integer> of(int from, int to) {
+        return of(from, to, integer -> integer + 1);
+    }
+
+    public static Range<Double> of(double from, double to) {
+        return of(from, to, aDouble -> aDouble + 0.1);
+    }
+
+    public static Range<Float> of(float from, float to) {
+        return of(from, to, aFloat -> aFloat + 0.1f);
+    }
+
+    public static Range<Short> of(short from, short to) {
+        return of(from, to, aShort -> (short) (aShort + 1));
+    }
+
+    public static Range<Byte> of(byte from, byte to) {
+        return of(from, to, aByte -> (byte) (aByte + 1));
+    }
+
+    public static Range<Long> of(long from, long to) {
+        return of(from, to, aLong -> (long) (aLong + 1));
     }
 }
